ea39b12eac5010cec3f44ef6923e8d98efb6ec3a
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-19 20:33:06 +0300

    🔥 Updated: Plugin gitlab-ci.yml file.

--- woocommerce-product-voting-addon.php
+++ woocommerce-product-voting-addon.php
@@ -98,33 +98,3 @@ function init_wpva_addon() {
 
 add_action( 'init', __NAMESPACE__ . '\\init_wpva_addon' );
 require_once __DIR__ . '/includes/Widgets/index.php';
-return;
-
-define( 'BPVMWPVA_PARENT_PLUGIN_INSTALLED_VERSION', get_option( 'bwl_pvm_plugin_version' ) );
-define( 'BPVMWPVA_ADDON_PARENT_PLUGIN_TITLE', 'BWL Pro Voting Manager' );
-define( 'BPVMWPVA_ADDON_TITLE', 'WooCommerce Product Voting Addon' );
-define( 'BPVMWPVA_PARENT_PLUGIN_REQUIRED_VERSION', '1.3.0' ); // change plugin required version in here.
-define( 'BPVMWPVA_ADDON_CURRENT_VERSION', '1.1.4' ); // change plugin current version in here.
-
-define( 'BPVMWPVA_ADDON_ROOT_FILE', 'woocommerce-product-voting-addon.php' ); // use for the meta info.
-
-define( 'BPVMWPVA_PATH', plugin_dir_path( __FILE__ ) );
-define( 'BPVMWPVA_DIR', plugins_url() . '/woocommerce-product-voting-addon/' );
-define( 'BPVMWPVA_UPDATER_SLUG', plugin_basename( __FILE__ ) );
-define( 'BPVMWPVA_CC_ID', '14564197' );
-define( 'BPVMWPVA_INSTALLATION_TAG', 'wpva_bpvm_installation_' . str_replace( '.', '_', BPVMWPVA_ADDON_CURRENT_VERSION ) );
-
-define( 'BPVMWPVA_PARENT_PLUGIN_PURCHASE_STATUS', get_option( 'bpvm_purchase_verified' ) == 1 ? 1 : 0 );
-
-require_once BPVMWPVA_PATH . 'includes/public/class-wpva-addon.php';
-
-register_activation_hook( __FILE__, [ 'BPVM_Wpva', 'activate' ] );
-register_deactivation_hook( __FILE__, [ 'BPVM_Wpva', 'deactivate' ] );
-
-add_action( 'plugins_loaded', [ 'BPVM_Wpva', 'get_instance' ] );
-
-if ( is_admin() ) {
-
-    include_once BPVMWPVA_PATH . 'includes/admin/class-wpva-addon-admin.php';
-    add_action( 'plugins_loaded', [ 'BPVM_Wpva_Admin', 'get_instance' ] );
-}
